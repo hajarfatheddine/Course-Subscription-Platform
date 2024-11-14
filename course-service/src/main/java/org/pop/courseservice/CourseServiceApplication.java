0cceb1dc761048c7df266bca1214ec45b4537b2c
hajar fatheddine <fatheddinehajar@gmail.com>
2024-11-14 14:34:46 +0100

    Add initial microservices

---
+++ course-service/src/main/java/org/pop/courseservice/CourseServiceApplication.java
@@ -0,0 +1,11 @@
+package org.pop.courseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class CourseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseServiceApplication.class,args);
+    }
+}
